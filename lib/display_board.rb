78ee546d8f9fe210e4a530d442b4ebc8dc679554
Betzalel Levi <betzalelleviva@gmail.com>
2020-05-11 23:40:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["
+          "" | ""|""
+          -----------
+          "" |"" |""
+          -----------
+          "" | ""|""
+                     "]
+
+
+
+def display_board(board)
+  puts "#{board}"
+end
+puts(board)
+puts" o #{board[0]}"
