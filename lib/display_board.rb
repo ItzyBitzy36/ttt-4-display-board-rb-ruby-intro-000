62527053f297a91b719d3ebb0f9b4b6e1a40e6f2
Betzalel Levi <betzalelleviva@gmail.com>
2020-05-11 23:41:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,12 @@
-board = ["
-          "" | ""|""
-          -----------
-          "" |"" |""
-          -----------
-          "" | ""|""
-                     "]
-
-
-
+board = ["x ", "o ", " x", "o", "x", " x", "o", "x ", "o"]
 def display_board(board)
-  puts "#{board}"
 end
-puts(board)
-puts" o #{board[0]}"
+
+
+
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
